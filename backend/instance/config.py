efc63b28cff2eada2504f65af6d8fcb501487e25
Arktetra <sameershrestha.sm@gmail.com>
2025-01-25 11:15:56 +0545

    delete config.py

--- backend/instance/config.py
+++
@@ -1,6 +0,0 @@
-DB_HOST = "postgresql://concept_db:eszJb8N8NOxcaeXn94WFmbViP5lEmX8S@dpg-cu5ngjt2ng1s73bh2erg-a.oregon-postgres.render.com/concept_db"
-DB_NAME = "concept_db"
-DB_USER = "concept_db"
-DB_PASSWORD = "eszJb8N8NOxcaeXn94WFmbViP5lEmX8S"
-DB_PORT = 5432
-# SECRET_KEY

8608aaa1e62db18eb8b63a0856290c553d5555e7
Arktetra <sameershrestha.sm@gmail.com>
2025-01-26 22:04:58 +0545

    move the content of the root +page.svelte to home

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,8 +1,2 @@
 <script lang="ts">
-  import page from "./login/+page.svelte";
-
-  import Post from "./components/PostPreview.svelte";
-  import Posts from "./components/Posts.svelte";
 </script>
-
-<Posts />
---
+++ frontend/src/routes/home/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import Posts from "../components/Posts.svelte";
+</script>
+
+<Posts />

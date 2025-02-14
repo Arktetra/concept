99143e3a70ad820c98038252687a28d9da14482f
Arktetra <sameershrestha.sm@gmail.com>
2025-02-14 21:29:28 +0545

    add more columns in the returned data

--- backend/posts.py
+++ backend/posts.py
@@ -2,11 +2,11 @@ from flask import Blueprint, request
 
 from backend.models.posts import Posts
 
-bp = Blueprint("concept", __name__, url_prefix="/blog/posts")
+bp = Blueprint("post", __name__, url_prefix="/posts")
 
 
 @bp.route("/get", methods=["POST"])
-def get_concept() -> dict:
+def get_post() -> dict:
     if request.method == "POST":
         post_id = request.json["slug"]
 

12e2520e4cf69a2ce31d4ee999168fc119460aea
Arktetra <sameershrestha.sm@gmail.com>
2025-02-15 20:33:15 +0545

    add callback for adding concepts

--- frontend/src/callbacks.svelte.ts
+++ frontend/src/callbacks.svelte.ts
@@ -1,3 +1,5 @@
+import { concept } from "./state.svelte";
+
 export const getConcepts = async () => {
     try {
         const res = await fetch("concepts/get");
@@ -11,6 +13,29 @@ export const getConcepts = async () => {
         console.log(data);
 
         return data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export const addConcepts = async () => {
+    try {
+        const res = await fetch("/concepts/add", {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: concept.title,
+                abstract: concept.abstract,
+                content: concept.content
+            })
+        });
+
+        if (!res.ok) {
+            const error = await res.json();
+            throw new Error(error.error)
+        }
     } catch (err) {
         console.log(err);
     }

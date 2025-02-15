29571996b19186b0b6907a5a5972bfc2ee03f176
Arktetra <sameershrestha.sm@gmail.com>
2025-02-15 20:32:53 +0545

    add state related to creating posts

--- frontend/src/state.svelte.ts
+++ frontend/src/state.svelte.ts
@@ -1,3 +1,13 @@
 export const user = $state({
     session: false
 });
+
+export const create = $state({
+    clicked: false
+});
+
+export const concept = $state({
+    title: "",
+    abstract: "",
+    content: ""
+});

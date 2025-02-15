eff435b6146934c9ba7bb966c9053093a95e830d
Arktetra <sameershrestha.sm@gmail.com>
2025-02-15 20:33:37 +0545

    add new urls and fix existing ones

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,9 @@ export default defineConfig({
 		proxy: {
 			'/auth/login': 'http://127.0.0.1:5000',
 			'/auth/register': 'http://127.0.0.1:5000',
+			'/categories/get': 'http://127.0.0.1:5000',
 			'/concepts/get': 'http://127.0.0.1:5000',
+			'/concepts/add': 'http://127.0.0.1:5000',
 			'/posts/get': 'http://127.0.0.1:5000',
 		}
 	}

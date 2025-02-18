10a0ac8774ea81fc5c2f200fd3d6c1a260eff2ce
Arktetra <sameershrestha.sm@gmail.com>
2025-02-18 18:59:56 +0545

    change: display pop-up to create post or category

--- frontend/src/routes/concept/create/+page.svelte
+++ frontend/src/routes/concept/create/+page.svelte
@@ -1,172 +1,71 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import { mdToHtml } from "../../../converter";
-    import { concept, create } from "../../../state.svelte";
-
-    onMount(() => {
-        create.clicked = true;
-
-        return () => create.clicked = false;
-    });
 </script>
 
-<article class="concept">
-    <div class="source">
-        <div class="title-block">
-            <textarea
-                class="title internal"
-                placeholder="Title"
-                bind:value={concept.title}
-            ></textarea>
-            <textarea
-                class="abstract internal"
-                placeholder="Abstract"
-                bind:value={concept.abstract}
-            ></textarea>
-        </div>
-        <div class="body">
-            <textarea class="content internal"
-                placeholder="Your concept..."
-                bind:value={concept.content}
-            ></textarea>
+<div class="popup-overlay" id="popupOverlay">
+    <div class="popup" id="popup">
+        <span class="close" id="closePopup">&times;</span>
+        <div class="popup-content">
+            <p>Select one of the following options:</p>
+            <a id="createPost" href="/concept/create/post">Create Post</a>
+            <a id="createCategory" href="/concept/create/category">Create Category</a>
         </div>
     </div>
-    <div class="render">
-        <div class="title-block">
-            <h1 class="title internal">{concept.title}</h1>
-            <p class="abstract internal">{concept.abstract}</p>
-        </div>
-        <div
-            class="body internal"
-            style="
-                display: flex;
-                flex-direction: column;
-                width: 100%;
-                padding-bottom: 250px;">
-            {@html mdToHtml(concept.content)}
-        </div>
-    </div>
-
-</article>
+</div>
 
 <style>
-    .concept {
-        display: flex;
-    }
-
-    p, h1, textarea {
-        margin: 0;
-        padding: 0;
-    }
-
-    .source {
-        display: flex;
-        flex-direction: column;
-        border-right: 1px solid rgb(0, 0, 0, 0.2);
-    }
-
-    .render {
-        border-left: 1px solid rgb(0, 0, 0, 0.2);
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-
-    .render .body {
+    .popup-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
         width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
     }
 
-    .render .body :global {
-        * {
-            width: 40vw;
-        }
+    .popup {
+        font-family: Arial, Helvetica, sans-serif;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background: #fff;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
     }
 
-    .title-block {
-        padding: 1rem 0rem 1rem 0rem;
-        display: flex;
-        width: 100%;
-        flex-direction: column;
-        align-items: center;
-        border-bottom: 1px solid rgb(0, 0, 0, 0.2);
+    .popup-content {
+        text-align: center;
     }
 
-    textarea {
-        resize: none;
-        overflow: hidden;
-        field-sizing: content;
+    .popup .close {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        font-size: 20px;
+        cursor: pointer;
+        color: #333;
+    }
+
+    .popup a {
+        background-color: rgb(38, 84, 184);
+        color: #fff;
+        padding: 10px 0px;
         border: none;
-    }
-
-    textarea:focus {
-        outline: none;
-    }
-
-    .title {
-        padding: 1rem 0rem 1rem 0;
-        font-size: 48px;
-        margin-bottom: 0px;
-        font-weight: bold;
-    }
-
-    .abstract {
-        font-size: 18px;
-    }
-
-    .body {
+        border-radius: 4px;
+        cursor: pointer;
+        font-size: 16px;
         display: flex;
         flex-direction: column;
-        align-items: center;
-        padding-bottom: 250px;
+        text-decoration: none;
+        width: 100%;
     }
 
-    .content, .render>.body {
-        padding: 1rem 0rem 1rem 0;
-        font-size: 18px;
+    .popup a:hover {
+        opacity: 0.9;
     }
 
-    .source, .render {
-        width: 50vw;
-    }
-
-    .source, .render {
-        height: 90vh;
-        max-height: 90vh;
-        overflow-y: scroll;
-    }
-
-    .internal {
-        width: 40vw;
-        font-family: 'Times New Roman', Times, serif;
-    }
-
-    .render .title {
-        overflow-wrap: break-word;
-    }
-
-    @media only screen and (max-width: 768px) {
-        .internal {
-            width: 40vw;
-        }
-
-
-    }
-
-    :global(html) {
-        overflow-x: hidden;
-        overflow-y: hidden;
-    }
-
-    ::-webkit-scrollbar {
-        width: 5px;
-    }
-
-    ::-webkit-scrollbar-track {
-        background: #f1f1f1;
-    }
-
-    ::-webkit-scrollbar-thumb {
-        background: #888;
-        border-radius: 5px;
+    #createCategory {
+        margin-top: 10px;
     }
 </style>
---
+++ frontend/src/routes/concept/create/category/+page.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { concept, create } from "../../../../state.svelte";
+
+
+    onMount(() => {
+        create.clicked = true;
+        create.type = "category";
+
+        return () => create.clicked = false;
+    })
+</script>
+
+<article class="category">
+    <div class="title-block">
+        <textarea
+            class="title internal"
+            placeholder="Title"
+            bind:value={concept.title}
+        ></textarea>
+        <textarea
+            class="abstract internal"
+            placeholder="Abstract"
+            bind:value={concept.abstract}
+        ></textarea>
+    </div>
+</article>
+
+<style>
+    textarea {
+        margin: 0;
+        padding: 0;
+        resize: none;
+        overflow: hidden;
+        field-sizing: content;
+        border: none;
+    }
+
+    textarea:focus {
+        outline: none;
+    }
+
+    .abstract {
+        font-size: 18px;
+    }
+
+    .category {
+        display: flex;
+        justify-content: center;
+    }
+
+    .internal {
+        width: 50vw;
+        font-family: 'Times New Roman', Times, serif;
+    }
+
+    .title-block {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .title {
+        padding: 1rem 0rem 1rem 0;
+        font-size: 48px;
+        margin-bottom: 0px;
+        font-weight: bold;
+        overflow-wrap: break-word;
+    }
+
+    @media only screen and (max-width: 786px) {
+        .internal {
+            width: 90vw;
+        }
+    }
+</style>

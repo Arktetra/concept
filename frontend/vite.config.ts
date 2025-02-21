c36189f5437e7b664019ae10b207c9191cc435ad
Arktetra <sameershrestha.sm@gmail.com>
2025-02-21 17:20:22 +0545

    update routes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,9 +8,11 @@ export default defineConfig({
 			'/accounts/login': 'http://127.0.0.1:5000',
 			'/accounts/register': 'http://127.0.0.1:5000',
 			'/categories/get': 'http://127.0.0.1:5000',
+			'/comments/add': 'http://127.0.0.1:5000',
 			'/concepts/get': 'http://127.0.0.1:5000',
 			'/concepts/add': 'http://127.0.0.1:5000',
 			'/posts/get': 'http://127.0.0.1:5000',
+			'/posts/comments': 'http://127.0.0.1:5000',
 		}
 	}
 });

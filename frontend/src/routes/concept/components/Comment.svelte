2a7ebfd63dc6e925f6f9f24752a140dde9c0939c
Arktetra <sameershrestha.sm@gmail.com>
2025-02-21 17:19:18 +0545

    add component for displaying a comment

---
+++ frontend/src/routes/concept/components/Comment.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    let { comment } : {
+        comment: {
+            user_name: string,
+            comment_text: string,
+            created_at: string
+        }
+    } = $props()
+</script>
+
+<div class="comment-container">
+    <div class="metadata">
+        <h3 class="user-name">{comment.user_name}</h3>
+        <p class="date">{comment.created_at}</p>
+    </div>
+    <p>{comment.comment_text}</p>
+</div>
+
+<style>
+    h3, p {
+        margin: 0px;
+        padding: 0px;
+    }
+
+    .comment-container {
+        border-left: 1px solid rgb(0, 0, 0, 0.2);
+        padding-left: 1em;
+        margin-bottom: 1em;
+    }
+
+    .comment-container h3 {
+        display: block;
+    }
+
+    .metadata {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        margin-bottom: 1em;
+    }
+
+    .date {
+        color: rgb(0, 0, 0, 0.7);
+    }
+</style>

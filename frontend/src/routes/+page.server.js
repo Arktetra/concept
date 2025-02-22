3dc3f610fab2b028f2a933b927ab2eeecd586066
Arktetra <sameershrestha.sm@gmail.com>
2025-02-22 10:23:30 +0545

    change landing page to /concept

--- frontend/src/routes/+page.server.js
+++ frontend/src/routes/+page.server.js
@@ -1,10 +1,5 @@
 import { redirect } from "@sveltejs/kit";
-import { user } from "../state.svelte";
 
 export function load() {
-    if (!user.session) {
-        redirect(307, "/concept/login")
-    } else {
-        redirect(307, "/concept")
-    }
+    redirect(307, "/concept");
 }
